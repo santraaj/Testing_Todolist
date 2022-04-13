368cabcbddb52487ecca8cf3f48cb456a1b98ce9
santraaj <santra.jantunen@gmail.com>
2022-04-13 23:46:57 +0300

    Tehtävä valmis

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -6,4 +8,16 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('add todo', () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '29.04.2022' } });
+  const button = screen.getByText('Add');
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
 });
